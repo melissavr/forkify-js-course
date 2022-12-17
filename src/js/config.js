b8a1d547fac41ac98c516a9e25e1ee9bef35f8e1
melissavr <63030172+melissavr@users.noreply.github.com>
2022-12-17 21:45:36 +1300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '6ba8c5bf-2e25-4ab3-93ec-5333713382a8';
+export const MODAL_CLOSE_SECONDS = 2.5;
